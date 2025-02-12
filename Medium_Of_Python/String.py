d29ce91dac9170b4309da20bc237ebd66ceeb9b5
saptarshi chowdhury <142312774+1saptarshi@users.noreply.github.com>
2025-02-12 21:26:55 +0530

    add string

---
+++ Medium_Of_Python/String.py
@@ -0,0 +1,67 @@
+#Strings in Python
+
+#A string in Python is a sequence of characters enclosed in 
+# single ('), double ("), or triple (''' or """) quotes.
+
+# Declaring Strings
+
+# Single and Double Quotes
+str1 = 'Hello'
+str2 = "Python"
+
+print(str1)  # Output: Hello
+print(str2)  # Output: Python
+
+
+#2. Quotes Inside Quotes
+
+#You can use different types of quotes to include quotes inside a string.
+
+#Using Quotes Inside a String
+
+# Using double quotes inside single quotes
+str1 = 'He said, "Python is awesome!"'
+
+# Using single quotes inside double quotes
+str2 = "It's a beautiful day"
+
+# Using escape character (\) to include same type of quotes
+str3 = 'It\'s a sunny day'
+str4 = "He said, \"Python is amazing!\""
+
+print(str1)  # Output: He said, "Python is awesome!"
+print(str2)  # Output: It's a beautiful day
+print(str3)  # Output: It's a sunny day
+print(str4)  # Output: He said, "Python is amazing!"
+
+
+#3. Assign String to a Variable
+
+#Instead of using direct strings, assign them to variables.
+
+#Assigning a String to a Variable
+
+greeting = "Hello, World!"
+print(greeting)  # Output: Hello, World!
+
+#4. Multiline Strings
+
+#Use triple quotes (''' or """) to define a string spanning multiple lines.
+
+# Creating Multiline Strings
+
+message = """This is a
+multiline string
+in Python."""
+print(message)
+
+#5. Strings are Arrays
+#Strings behave like arrays, meaning you can access individual characters using indexing.
+
+#Accessing String Characters
+
+text = "Python"
+print(text[3])  # Output: P
+print(text[5])  # Output: h
+
+
--- Practice/py_prac#2.py
+++ Practice/py_prac#2.py
@@ -124,18 +124,18 @@ print(type(w))
 
 #answer
 
-num1 = float(input("Enter 1st number: "))  # Added a colon for better readability
-num2 = float(input("Enter 2nd number: "))  # Added a space for better formatting
+# num1 = float(input("Enter 1st number: "))  # Added a colon for better readability
+# num2 = float(input("Enter 2nd number: "))  # Added a space for better formatting
 
-# Performing arithmetic operations
-addition = num1 + num2
-subtraction = num1 - num2
-multiplication = num1 * num2
+# # Performing arithmetic operations
+# addition = num1 + num2
+# subtraction = num1 - num2
+# multiplication = num1 * num2
 
-# Displaying results with labels
-print("Addition:", addition)
-print("Subtraction:", subtraction)
-print("Multiplication:", multiplication)
+# # Displaying results with labels
+# print("Addition:", addition)
+# print("Subtraction:", subtraction)
+# print("Multiplication:", multiplication)
 
 # Number Type Conversion
 # Convert:
@@ -143,6 +143,13 @@ print("Multiplication:", multiplication)
 # 10 into a float
 # 5.99 into an integer
 # "42" into an integer
+
+ #Convert 10 into a Float
+num=10
+float_num =float(num)
+print(type(float_num))
+
+
 
 
  

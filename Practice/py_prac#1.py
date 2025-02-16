4b8460f301032e7c6ca6eb23e68be3c741f51404
saptarshi chowdhury <142312774+1saptarshi@users.noreply.github.com>
2025-02-16 14:02:06 +0530

    add ptactice

--- Practice/py_prac#1.py
+++ Practice/py_prac#1.py
@@ -20,3 +20,35 @@ ca ={'BMW','BBT','LAMBERGINI','BUATTI','ROLLS ROYLES','BENTLY'}
 print(type(ca))
 
  
+s = "saptarshi"
+print(s*7)
+ 
+num =range(10)
+print(num)
+
+
+frozen_numbers = frozenset([1, 2, 3, 4, 5])
+print(type(frozen_numbers))  # Output: <class 'frozenset'>
+
+
+
+num = frozenset([1,2,3,4,5,6,7,8,9,])
+print(num)
+
+
+
+info ={"Name":"saptarshi","Age":23}
+print(type(info))
+print(info)
+
+
+
+byte_data = b"saptarshi"
+print(type(byte_data))  # Output: <class 'bytes'>
+
+
+empty_value = None
+print(type(empty_value))  # Output: <class 'NoneType'>
+
+a= None
+print(type(a))

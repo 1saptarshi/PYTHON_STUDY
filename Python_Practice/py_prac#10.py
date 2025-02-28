bfbfd76c820d85ccee67e3ee8350889fbee51138
saptarshi chowdhury <142312774+1saptarshi@users.noreply.github.com>
2025-02-28 20:17:06 +0530

    update

--- Python_Practice/py_prac#10.py
+++ Python_Practice/py_prac#10.py
@@ -1,8 +1,15 @@
 
 count = 1
-while count <=10:
+while count <=2:
     print("itaration:",count)
     count +=1
 
 
+
+cou = 1
+while cou <=5:
+  if cou ==3:
+    break
+  print("output:",cou)
+  cou+=1
     
